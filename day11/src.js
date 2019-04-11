c4e728ff15c2cdda2c821c193506c69736230acf
Christian Lassen <christian@mudbath.com.au>
2019-04-11 21:53:08 +1000

    Add progress indicator, volume, playbackrate, skip, and play buttons to custom video player

--- day11/src.js
+++ day11/src.js
@@ -2,8 +2,26 @@ var player = document.querySelector('video');
 var playButton = document.querySelector('button.player__button.toggle');
 var skipBackward = document.querySelector('button[data-skip="-10"]');
 var skipForward = document.querySelector('button[data-skip="25"]');
+var volume = document.querySelector('input[name=volume]');
+var playBackRate = document.querySelector('input[name=playbackRate]');
+var progress = document.querySelector('.progress__filled');
+var sliderInterval;
 
-playButton.addEventListener('click', () => {
+volume.value = player.volume;
+playbackRate.value = player.playbackRate;
+
+function handleOnPlaying() {
+    sliderInterval = setInterval(() => {
+        console.log((player.currentTime / player.duration) * 100)
+        progress.style.flexBasis = `${(player.currentTime / player.duration)*100}%`;
+    }, 1000);
+}
+
+function handleOnPause() {
+
+}
+
+function togglePlay() {
     if(player.paused) {
         player.play();
         playButton.innerHTML = '⏸️';
@@ -12,12 +30,20 @@ playButton.addEventListener('click', () => {
         player.pause();
         playButton.textContent = '▶️';
     }
-});
+}
 
-skipForward.addEventListener('click', () => {
+function handleVolumeChange() {
+    player.volume = volume.value;
+}
+
+function handlePlaybackRateChange(event) {
+   player.playbackRate = event.srcElement.value;
+}
+
+function handleSkipForward() {
     player.currentTime += 25;
-})
+}
 
-skipBackward.addEventListener('click', () => {
+function handleSkipBackward() {
     player.currentTime -= 10;
-})
+}
